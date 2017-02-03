d9ceb48d8ab00799d6033e7a600f66d95bc5f2ec
Ilias Basha <iliasbasha@Iliass-MacBook-Pro.local>
2017-02-03 09:40:34 -0700

    created view controller added properties to model

---
+++ Sources/SMBMovies.h
@@ -0,0 +1,20 @@
+//
+//  SMBMovies.h
+//  MovieSearch
+//
+//  Created by Ilias Basha on 2/3/17.
+//  Copyright © 2017 Open Reel Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMBMovies : NSObject
+
+@property (nonatomic, readonly, copy) NSString *title;
+@property (nonatomic, readonly) NSInteger rating;
+@property (nonatomic, readonly, copy) NSString *summary;
+
+-(instancetype)initWithTitle:(NSString *)title rating:(NSInteger)rating summary:(NSString *)summary;
+
+
+@end
---
+++ Sources/SMBMovies.m
@@ -0,0 +1,16 @@
+//
+//  SMBMovies.m
+//  MovieSearch
+//
+//  Created by Ilias Basha on 2/3/17.
+//  Copyright © 2017 Open Reel Software. All rights reserved.
+//
+
+#import "SMBMovies.h"
+
+@implementation SMBMovies
+
+
+
+
+@end
